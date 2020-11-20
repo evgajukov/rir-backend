97a67582017354a7249afadbf1be5e9be521a2cb
Timur Evgazhukov <evgajukov@gmail.com>
2020-11-20 12:53:37 +0300

    Мнемонику для смс Dom24x7 сотовый оператор согласовал, нужно протестировать на ПРОМ

--- src/lib/smsc/index.ts
+++ src/lib/smsc/index.ts
@@ -10,7 +10,7 @@ export default class SMSC {
     password: "Tck89mta7s3z",
     charset: "utf-8",
     translit: 0,
-    sender: "SKHUB"
+    sender: "Dom24x7"
   };
 
   static async send(phones: Array<string>, message: string) {

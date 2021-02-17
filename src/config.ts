e49e36bfb7be7c30d7e4f3804351e70d5e634fa4
Timur Evgazhukov <evgajukov@gmail.com>
2021-02-17 21:02:10 +0300

    вынесли часть конфигурации в .env

--- src/config.ts
+++ src/config.ts
@@ -7,23 +7,14 @@ const DEFAULT_ENV_NAME = ENV_NAME.DEV;
 const env = process.env.NODE_ENV || DEFAULT_ENV_NAME;
 const db = require("./db/credentials")[env];
 
-const plogicId = env == DEFAULT_ENV_NAME ? "srv00" : process.env.PLOGIC_ID;
-
 const config = {
   env,
   db: db,
   redis: {
-    caching: false,
-    host: "136.144.29.95",
-    login: "admin",
-    password: "AENgcz02491"
-  },
-  kue_web: {
-    prefix: "dom24x7_kue_web",
-  },
-  kue_pl: {
-    prefix_without_srv: "dom24x7_kue_pl",
-    prefix: `dom24x7_kue_pl${plogicId != null ? "_" + plogicId : ""}`,
+    caching: process.env.CACHE_FLAG == "true",
+    host: process.env.CACHE_REDIS_HOST,
+    login: process.env.CACHE_REDIS_LOGIN,
+    password: process.env.CACHE_REDIS_PASSWORD
   }
 };
 

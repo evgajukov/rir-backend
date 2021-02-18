08e72d03b50459497138ad372726a69af8aa8b6c
Timur Evgazhukov <evgajukov@gmail.com>
2021-02-18 12:11:03 +0300

    вывели настройки БД из репозитория

--- src/db/credentials.ts
+++ src/db/credentials.ts
@@ -1,2 +1,18 @@
-const dbConfig = require("./config/config.json");
-module.exports = dbConfig;
+module.exports = {
+  development: {
+    dialect: process.env.DB_DIALECT_DEVELOPMENT,
+    username: process.env.DB_USERNAME_DEVELOPMENT,
+    password: process.env.DB_PASSWORD_DEVELOPMENT,
+    host: process.env.DB_HOST_DEVELOPMENT,
+    port: process.env.DB_PORT_DEVELOPMENT,
+    database: process.env.DB_DATABASE_DEVELOPMENT
+  },
+  production: {
+    dialect: process.env.DB_DIALECT_PRODUCTION,
+    username: process.env.DB_USERNAME_PRODUCTION,
+    password: process.env.DB_PASSWORD_PRODUCTION,
+    host: process.env.DB_HOST_PRODUCTION,
+    port: process.env.DB_PORT_PRODUCTION,
+    database: process.env.DB_DATABASE_PRODUCTION
+  }
+};
